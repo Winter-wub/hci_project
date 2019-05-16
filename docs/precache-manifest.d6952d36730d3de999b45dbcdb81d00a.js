42ff6cca2fce480f97d2ada1fc7fb5f7d7a67e0d
StratosBlur <wave.onlyone@gmail.com>
2019-05-16 23:05:12 +0700

    Update docs folder test github page

---
+++ docs/precache-manifest.d6952d36730d3de999b45dbcdb81d00a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b41f083621e6f36414fe10d3bfbb8125",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a65deb32dd05e74f6699",
+    "url": "/static/js/2.fffd76f3.chunk.js"
+  },
+  {
+    "revision": "06f14cc2c497a70da6c1",
+    "url": "/static/js/main.d993b854.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
