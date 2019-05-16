03460bcdbc7f740def5c454ccbd54af8630a4355
StratosBlur <wave.onlyone@gmail.com>
2019-05-16 10:44:45 +0700

    remove unused css

--- src/components/checkbox.js
+++ src/components/checkbox.js
@@ -1,30 +1,18 @@
 import React from 'react';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import { makeStyles } from '@material-ui/styles/';
-const useStyles = makeStyles(theme => ({
-	checkbox: {
-		color: 'white',
-	},
-	'&$checked': {
-		color: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-	},
-}));
+
 function CheckboxComponent({
 	label = '',
 	checked,
 	onChange = () => {},
 	value,
 }) {
-	const classes = useStyles();
+	
 	return (
 		<FormControlLabel
 			control={
 				<Checkbox
-					className={{
-						root: classes.checkbox,
-						checked: classes.checked,
-					}}
 					checked={checked}
 					onChange={onChange}
 					value={value}
